a3a6c5e5a2412ae0c6ab317f47e68b2fd467e992
bishri <bishri60@gmail.com>
2025-02-11 14:54:27 +0300

    fix: Change return_date field type to Date in Loan doctype and update type hints in loan.py

--- library_management/library_management/doctype/loan/loan.py
+++ library_management/library_management/doctype/loan/loan.py
@@ -6,4 +6,20 @@ from frappe.model.document import Document
 
 
 class Loan(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		book: DF.Link
+		due_date: DF.Date | None
+		loan_date: DF.Date | None
+		member: DF.Link
+		return_date: DF.Date | None
+		status: DF.Literal["Borrowed", "Returned"]
+	# end: auto-generated types
+
 	pass
