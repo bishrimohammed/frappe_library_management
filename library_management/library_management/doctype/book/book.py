9b3ac15696959467922fd6fbf307ad6af9242a3c
bishri <bishri60@gmail.com>
2025-02-11 19:24:51 +0300

    fix: Update book status options and add type hints for Book doctype fields

--- library_management/library_management/doctype/book/book.py
+++ library_management/library_management/doctype/book/book.py
@@ -6,4 +6,19 @@ from frappe.model.document import Document
 
 
 class Book(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		author: DF.Data | None
+		isbn: DF.Data | None
+		publish_date: DF.Date | None
+		status: DF.Literal["Available", "Borrowed"]
+		title: DF.Data
+	# end: auto-generated types
+
 	pass
