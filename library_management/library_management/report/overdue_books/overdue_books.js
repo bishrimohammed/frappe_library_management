adf516aab791e8128d1813678bc199d5346de767
bishri <bishri60@gmail.com>
2025-02-19 14:58:21 +0300

    feat: Add overdue books report with script execution and updated query

---
+++ library_management/library_management/report/overdue_books/overdue_books.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2025, Bishri Mohammed and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Overdue books"] = {
+	filters: [
+		// {
+		// 	"fieldname": "my_filter",
+		// 	"label": __("My Filter"),
+		// 	"fieldtype": "Data",
+		// 	"reqd": 1,
+		// },
+	],
+};
